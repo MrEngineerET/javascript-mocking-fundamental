f3cd5af3cb421eb51a5309db00990513876ee5e7
Biruk Berhanu <meetbirukberhanu@gmail.com>
2022-11-16 08:56:05 +0300

    implement inline-mock-module from without framework

---
+++ src/no-framework/inline-module-mock.js
@@ -0,0 +1,34 @@
+const utilsPath = require.resolve("../utils");
+require.cache[utilsPath] = {
+  id: utilsPath,
+  filename: utilsPath,
+  loaded: true,
+  exports: {
+    getWinner: fn((p1, p2) => p1),
+  },
+};
+const assert = require("assert");
+const thumbWar = require("../thumb-war.js");
+const utils = require("../utils");
+
+function fn(impl) {
+  const mockFn = (...args) => {
+    mockFn.mock.calls.push(args);
+    return impl(...args);
+  };
+  mockFn.mock = { calls: [] };
+  return mockFn;
+}
+
+const playerOne = "Biruk Berhanu";
+const playerTwo = "Abraham Adamu";
+
+const winner = thumbWar(playerOne, playerTwo);
+assert.strictEqual(winner, playerOne);
+assert.deepStrictEqual(utils.getWinner.mock.calls, [
+  ["Biruk Berhanu", "Abraham Adamu"],
+  ["Biruk Berhanu", "Abraham Adamu"],
+]);
+
+//cleanup
+delete require.cache[utilsPath];
